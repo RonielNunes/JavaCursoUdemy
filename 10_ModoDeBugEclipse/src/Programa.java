582ea914ce6308328237c7f280f5cc3fe62adf6f
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-21 16:22:23 -0300

    debug

---
+++ 10_ModoDeBugEclipse/src/Programa.java
@@ -0,0 +1,15 @@
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Para marcar uma linha de breakpoint:
+			//• Run -> Toggle Breakpoint
+			//• Para iniciar o debug:
+			//• Botão direito na classe -> Debug as -> Java Application
+			//• Para executar uma linha:
+			//• F6
+			//• Para interromper o debug no icone vermelho
+	}
+
+}
