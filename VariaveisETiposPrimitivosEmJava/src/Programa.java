e71af77d95c429c5adc003bd2a4ecea3f450594c
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-21 15:22:35 -0300

    Variáveis e tipos primitivos em java

---
+++ VariaveisETiposPrimitivosEmJava/src/Programa.java
@@ -0,0 +1,16 @@
+
+public class Programa {
+
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = 12;
+		int i = 12;
+		long l = 12;
+		float f = 12;
+		double d = 12;
+		char c = 'o';
+		boolean bool = true;
+ 	
+	}
+
+}
