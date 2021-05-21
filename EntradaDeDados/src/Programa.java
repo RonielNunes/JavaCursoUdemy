204467639370812844fa8b70c4a4f13298dace39
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-21 15:28:06 -0300

    Entrada de dados

---
+++ EntradaDeDados/src/Programa.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int inteiro = teclado.nextInt();
+		float pontoFlutuante = teclado.nextFloat();
+		double pontoFlutunate2 = teclado.nextDouble();
+		String palavra = teclado.next();
+		String fraseComEspaco = teclado.nextLine();
+		//Entre outros...
+		
+		teclado.close();
+		
+		
+		
+	}
+
+}
