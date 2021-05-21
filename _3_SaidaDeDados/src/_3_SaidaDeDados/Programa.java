64d85dcf6e6c70b42eae22d3d193299466a417a4
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-21 15:38:35 -0300

    saida de dados

---
+++ _3_SaidaDeDados/src/_3_SaidaDeDados/Programa.java
@@ -0,0 +1,16 @@
+package _3_SaidaDeDados;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.print("Sem quebra de linha"); //sysout + ctral + espaço gera o comando de exibição
+		System.out.println("Com quebra de linha");
+		float numero  = 22.2312f;
+		System.out.printf("Com formatação %.2f%n", numero);
+		String palavra = "numero";
+		System.out.println("Concatenação de valores : " +  numero + " valor mostrado " + " String exibida " + palavra);
+		
+
+	}
+
+}
