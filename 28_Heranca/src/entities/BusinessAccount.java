602b38cc3f81345dc162436d4581315004ed55af
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-24 14:09:19 -0300

    herança, exemplo com protected

---
+++ 28_Heranca/src/entities/BusinessAccount.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class BusinessAccount extends Account{
+	private  Double loanLimit;
+	
+	public BusinessAccount() {
+		super();
+	}
+	
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(Double amount) {
+		if(amount <= loanLimit) {
+			deposit(amount);
+			//modificador de aceso protected
+			//Balance é da super classe mas não pode ser acessadpo por ser private
+			//logo para poder acessar ela devemos colocar "protected".
+			balanceWithAcess = 110.0; //Podemos acessar agora
+		}
+	}
+
+}
