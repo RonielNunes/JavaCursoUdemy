c82631970977a97c1c63956b1fbdd0c7d22e38e2
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-24 13:20:10 -0300

    Ecercício

--- 27.6_Exercicio3/src/application/Program.java
+++ 27.6_Exercicio3/src/application/Program.java
@@ -1,10 +1,67 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Email: ");
+		String email = sc.next();
+		
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date bithDate = sdf.parse(sc.next()); //String para Date com format
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next()); //String para enum
+		
+		//Momento se trada da hora atual, logo podemos intanciar no momento da ordem
+		//Cliente podemos criar um instanciação já no escopo da ordem
+		Order order = new Order(new Date(), status, new Client(name,email,bithDate));
+		
+		System.out.println();
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println();
+			System.out.println("Enter #" + (i+1) + "data: ");
+			sc.nextLine();
+			System.out.print("Product Name: ");
+			String productName = sc.nextLine();
+			
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+		
+			Product product = new Product(productName, productPrice);
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product); 
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println(order.toString());
+		
 	}
 
 }
